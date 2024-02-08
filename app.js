d6a92a4830af8aef6b4df14c5c747b8e4b7367b9
Vesimeu <pyoter.poliakov@yandex.ru>
2024-02-09 02:10:48 +0500

    Installing mongoBD

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+//connect BD
+mongoose.connect('mongodb://localhost:27017');
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log("Connected successfully to the local MongoDB database.");
+});
 
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
